28264f9ec812e55491d7530c3bdc11fbfc7cee34
rjshk013 <rjshk013@gmail.com>
2021-01-08 17:04:37 +0530

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,45 @@
+echo "*************************************************************************"
+echo 
+echo "                   BASIC SYSTEM INFO !!!!!                        "
+echo
+echo "*************************************************************************"
+echo
+echo
+echo MACHINE _________________________________________________________________
+dmidecode |head -n 50| grep -e "Vendor" -e "Manufacturer" -e "Product Name" -e "Serial Number"
+echo
+echo Computer Name ___________________________________________________________
+cat /etc/hostname 
+echo
+echo
+echo Operating System ________________________________________________________
+cat /proc/version
+read a
+clear
+echo
+echo
+echo
+echo
+echo Architecture ____________________________________________________________
+uname -m
+echo
+echo
+echo
+echo
+echo System UPtime ___________________________________________________________
+uptime
+echo
+echo
+echo
+echo CPU _____________________________________________________________________
+cat /proc/cpuinfo | grep -e "model" -e "cpu M"
+echo
+echo
+echo
+echo
+echo MEMORY __________________________________________________________________
+free -h
+echo
+echo
+
+
