e55f6a3737b5a89fce79f7994eaa41c89c246f2e
rjshk013 <rjshk013@gmail.com>
2021-01-08 16:10:57 +0530

    Update largest-files.sh

--- largest-files.sh
+++ largest-files.sh
@@ -1,1 +1,13 @@
 ls -l |awk '{print $5," ",$9}' | sort -n -r | more
+
+#To display the largest folders/files including the sub-directories, run:
+
+du -Sh | sort -rh | head -5
+
+#If you want to display the biggest file sizes only, then run the following command:
+
+find -type f -exec du -Sh {} + | sort -rh | head -n 5
+
+#To find the largest files in a particular location, just include the path besides the find command:
+
+find /home/ubuntuclient/Downloads/ -type f -exec du -Sh {} + | sort -rh | head -n 5
