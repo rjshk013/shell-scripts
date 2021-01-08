cb1a224471a896f9e9d4efe6897e7d41f7745b41
rjshk013 <rjshk013@gmail.com>
2021-01-08 16:34:28 +0530

    Update largest-files.sh

--- largest-files.sh
+++ largest-files.sh
@@ -11,3 +11,7 @@ find -type f -exec du -Sh {} + | sort -rh | head -n 5
 #To find the largest files in a particular location, just include the path besides the find command:
 
 find /home/ubuntuclient/Downloads/ -type f -exec du -Sh {} + | sort -rh | head -n 5
+
+#to find out largest folders only with human readable,chnage the counts accordingly
+
+ du -hsx -- * | sort -rh | head -20
