53919972b72a12ae023b7cf3cf878cee8710fa3d
rjshk013 <rjshk013@gmail.com>
2021-02-09 12:44:57 +0530

    Create apachetuning1.sh

---
+++ apachetuning1.sh
@@ -0,0 +1,14 @@
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_wmem=4096 65536 16777216
+net.ipv4.tcp_fin_timeout = 3
+net.ipv4.tcp_tw_recycle = 0
+net.core.netdev_max_backlog = 30000
+net.ipv4.tcp_no_metrics_save=1
+net.core.somaxconn = 262144
+net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_max_orphans = 262144
+net.ipv4.tcp_max_syn_backlog = 262144
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syn_retries = 2
