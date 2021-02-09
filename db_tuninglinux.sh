6571c17a0d70cea3cf48f1e3a198c07361d32cbe
rjshk013 <rjshk013@gmail.com>
2021-02-09 10:38:38 +0530

    Update db_tuninglinux.sh

--- db_tuninglinux.sh
+++ db_tuninglinux.sh
@@ -5,3 +5,16 @@ sysctl -a | grep swap
 sysctl -w vm.swappiness=1
 # Make persistent across reboots by appending this line to the /etc/sysctl.conf file
 echo “vm.swappiness=1” >> /etc/sysctl.conf
+# Set the swappiness value as root
+echo 1 > /proc/sys/vm/swappiness
+
+# Alternatively, using sysctl
+sysctl -w vm.swappiness=1
+
+# Verify the change
+cat /proc/sys/vm/swappiness
+1
+
+# Alternatively, using sysctl
+sysctl vm.swappiness
+vm.swappiness = 1
