e9c8aafb07ec6425eac6328148369701370106e4
rjshk013 <rjshk013@gmail.com>
2021-02-09 12:26:47 +0530

    Update usefulcommans.sh

--- usefulcommans.sh
+++ usefulcommans.sh
@@ -13,3 +13,8 @@ Linux Display or list only files
 
 ls -l | egrep -v '^d'
 ls -l | grep -v '^d'
+
+To find out the current system limit for file descriptors, 
+cat /proc/sys/fs/file-max
+To find out how many of the available file descriptors are being currently used,
+cat /proc/sys/fs/file-nr
