584b97e9dea1f8c878b43d3c589c9b6b4e88d6b5
rjshk013 <rjshk013@gmail.com>
2021-02-15 15:00:48 +0530

    Update cleanfiles.sh

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -7,3 +7,9 @@ find . -type d -empty
 find . -type d -empty -exec rmdir {} \;
 
 #Note: It is not recommended to remove empty directories from /etc/ or any other system directories.
+#Find empty files in the current directory using find -empty
+
+find . -type f -empty
+#To count the number of empty files under current directory, pipe the find command to wc -l
+
+find . -type f -empty | wc -l
