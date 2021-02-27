91394118ddcd8774fef4841248f9781a319e1176
rjshk013 <rjshk013@gmail.com>
2021-02-27 14:51:33 +0530

    Add files via upload

---
+++ eksctl_install-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#To install or upgrade eksctl on Linux using curl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+#Move the extracted binary to /usr/local/bin. 
+sudo mv /tmp/eksctl /usr/local/bin
+
+#Test that your installation was successful with the following command.
+
+eksctl version
